284b89c652264ac895da4f05bbff2b67fc826912
moen <moenr49@gmail.com>
2025-06-21 16:52:15 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(string){
+    if (string === undefined) {
+        return "This Saturday, I want to roller-skate!";
+    } else {
+        return `This Saturday, I want to ${string}!`;
+    }
+}
+
+function mondayWork(string){
+    if (string === undefined) {
+        return "This Monday, I will go to the office.";
+    } else {
+        return `This Monday, I will ${string}.`;
+    }
+}
+
+function wrapAdjective(flair) {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+
+}
